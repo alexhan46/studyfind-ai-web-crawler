44ae1cd43abdd985d238c25e5faa9e3a9431fa17
juntae <jkim3389@gatech.edu>
2020-09-18 11:01:41 -0400

    feat(crawling) : be able to get all of studyIDs existing in clincalTrial. However, needs to improve efficiency

--- id_crawling.py
+++ id_crawling.py
@@ -10,6 +10,15 @@ source = requests.get('https://clinicaltrials.gov/ct2/about-site/crawling').text
 soup = BeautifulSoup(source, 'lxml')
 
 td = soup.find('table')
-for a_tag in td.find_all('a', href=True):
-    print("Found URL:", a_tag['href'])
-# print(td.prettify())
+urls = td.find_all('a', href=True)
+studyID_list = []
+for a in urls:
+    parsedURL = "https://clinicaltrials.gov" + a.get('href')
+    new_source = requests.get(parsedURL).text
+    new_soup = BeautifulSoup(new_source, 'lxml')
+    table_for_ids = new_soup.find('table')
+    ids = table_for_ids.find_all('a')
+    
+    for id in ids:
+        studyID_list += [id.text]
+        print(studyID_list)
