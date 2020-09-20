150cf5ff3eaf56faf3f26de74bddc431d5a9fc77
juntae <jkim3389@gatech.edu>
2020-09-19 23:29:30 -0400

    feat(crawling): Be able to perform only scrapying newly added studies. much faster and efficiently than previous

--- id_crawling.py
+++ id_crawling.py
@@ -1,33 +1,65 @@
 from bs4 import BeautifulSoup
 import requests
-
-# getting html files from clinicaltrials.gov/ct2/about-site/crawling then grap the urls to each link.
-# then request to that url to scrap study ID
-
-
-source = requests.get('https://clinicaltrials.gov/ct2/about-site/crawling').text
-source_html_all = BeautifulSoup(source, 'lxml')
-
-td = source_html_all.find('table')
-urls_to_each_pages = td.find_all('a', href=True)
-studyID_list = []
-for a_tag in urls_to_each_pages:
-    parsedURL = "https://clinicaltrials.gov" + a_tag.get('href')
-    new_source = requests.get(parsedURL).text
-    source_html_per_page = BeautifulSoup(new_source, 'lxml')
-    table_for_ids = source_html_per_page.find('table')
-    ids = table_for_ids.find_all('a')
-    
-    for id in ids:
-        studyID_list += [id.text]
-        print(id.text)
+# adding fake User-Agent because our program is running in background
+from fake_useragent import UserAgent 
+import os.path
 
 
+# study_id_base_index refer to the starting studyid to be crawled
+def study_id_crawling(last_study_id):
+    ua = UserAgent()
+    header = {'user-agent':ua.chrome}
+    newly_added_studyIDs_list = []
+
+    # getting html files from clinicaltrials.gov/ct2/about-site/crawling then grap the urls linking each subpages.
+    source = requests.get('https://clinicaltrials.gov/ct2/about-site/crawling', headers=header, timeout= 30).text
+    soup = BeautifulSoup(source, 'lxml')
+
+    table = soup.find('table')
+
+    urls_to_each_pages = table.find_all('a', href=True)
+
+    for a_tag in urls_to_each_pages:
+        # Get range of studyID as list
+        range = (a_tag.next_sibling.split(" to "))
+        # Convert ['NCTxxxxxxx', 'NCTxxxxxxx'] to [xxxxxx, xxxxxx]
+        range = list(map(lambda x: int(x[3:]), range))
+
+        # search studies thta is created after given last_study_id
+        if(last_study_id<=range[0] or last_study_id<=range[1]):
+            
+            parsedURL = "https://clinicaltrials.gov" + a_tag.get('href')
+            new_source = requests.get(parsedURL, headers=header, timeout= 30).text
+            source_html_per_page = BeautifulSoup(new_source, 'lxml')
+            table_for_ids = source_html_per_page.find('table')
+            ids = table_for_ids.find_all('a')
+            
+            for id in ids:
+                id.string.replace_with(id.text[3:])
+                if (int(id.text) > last_study_id):
+                    newly_added_studyIDs_list += [id.text]
 
 
+    # (Test Purpose) exit early when they get 10000 study ids due to large volume 
+    #     if(len(new_studyID_list) > 10000):
+    #         break
+    return newly_added_studyIDs_list
 
-# Current Issues 
-#    scraping all studyIDs takes forever because there are 370,000 studies registered. 
-#    -> improve algorithms using datastucture(currently O(n^2)
-#    -> most of studies scraped are very old studies which is done long long ago. For example, Study NCT00000102 conducted at 2005
-#        Therefore, it needs to skip some old research. 
+
+# Testing purpose to generate result file to output folder
+def writeFileForTestPurpose(filename, list):
+    fileDir = os.path.dirname(os.path.realpath('__file__'))
+    if(not os.path.exists(fileDir+"/output")):
+        os.mkdir(fileDir+"/output")
+    relPath = 'output/'+filename+'.txt'
+    path = os.path.join(fileDir, relPath)
+    with open(path, 'w') as f:
+        for item in list:
+            f.write("%s\n" % item)
+
+# Test script with sample number of study id
+def main():
+    study_id_crawling(4557501)
+
+if __name__ == "__main__":
+    main()
